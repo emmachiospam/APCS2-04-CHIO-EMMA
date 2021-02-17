fcd20db985c97cb9a48bb6f8a4d2f4d77938ca2f
emmachio <echio20@stuy.edu>
2021-02-17 16:59:28 -0500

    fixed add and remove so that it also calculates places that other queens cannot go

--- 02-queens/QueenBoard.java
+++ 02-queens/QueenBoard.java
@@ -1,12 +1,54 @@
 public class QueenBoard {
   private int[][] board;
 
-  private boolean addQueen(int r, int c) {
-    return (board[r][c] == 0);
+  public boolean addQueen(int c, int r) {
+    int size = board.length;
+    if (board[r][c] == 0) {
+      board[r][c] = -1;
+      for(int j = 0; j < size; j++) {
+        if(j != r) {
+          board[j][c] = board[j][c] + 1;
+        }
+      }
+      int test = 0;
+      for(int i = c+1; i < size; i++) {
+        board[r][i] = board[r][i] + 1;
+        test++;
+        if(r - test > -1) {
+          board[r-test][i] = board[r - test][i] + 1;
+        }
+        if(r + test < size) {
+          board[r+test][i] = board[r + test][i] + 1;
+        }
+      }
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
-  private void removeQueen(int r, int c) {
-    board[r][c] = 0;
+  public void removeQueen(int c, int r) {
+    int size = board.length;
+    if (board[r][c] == -1) {
+      board[r][c] = 0;
+      for(int j = 0; j < size; j++) {
+        if(j != r) {
+          board[j][c] = board[j][c] - 1;
+        }
+      }
+      int test = 0;
+      for(int i = c+1; i < size; i++) {
+        board[r][i] = board[r][i] - 1;
+        test++;
+        if(r - test > -1) {
+          board[r-test][i] = board[r - test][i] - 1;
+        }
+        if(r + test < size) {
+          board[r+test][i] = board[r + test][i] - 1;
+        }
+      }
+    }
   }
 
   public QueenBoard(int size) {
@@ -21,6 +63,10 @@ public class QueenBoard {
         if(board[i][j] == -1) {
           result = result + "Q ";
         }
+        else if(board[i][j] > 0) {
+          result = result + "X ";
+          // result = result + board[i][j] + " ";
+        }
         else {
           result = result + "_ ";
         }
@@ -28,11 +74,40 @@ public class QueenBoard {
       if(board[i][size-1] == -1) {
         result = result + "Q \n";
       }
+      else if(board[i][size-1] > 0) {
+        result = result + "X \n";
+        // result = result + board[i][size-1] + "\n";
+      }
       else {
         result = result + "_ \n";
       }
     }
     return result;
   }
+
+  // public boolean solve() {
+  //   return solve(0, 0, 0);
+  // }
+  //
+  // public boolean solve(int column, int row, int pr) {
+  //   int size = board.length;
+  //   if((row == size - 1 && column == size - 1) && addQueen(column, row) == false) {
+  //     return false;
+  //   }
+  //   else if((row == size - 1 && column == size - 1) && addQueen(column, row) == true) {
+  //     return true;
+  //   }
+  //   else {
+  //     if(addQueen(column, row)) {
+  //       if(solve(column + 1, 0, row)) {
+  //         return true;
+  //       }
+  //       else {
+  //         return false;
+  //       }
+  //     }
+  //
+  //   }
+  // }
 
 }
