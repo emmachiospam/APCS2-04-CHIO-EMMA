b8ebe7c936a0a63e0b50c02abdc4ba9f56ab298c
emmachio <echio20@stuy.edu>
2021-02-22 23:42:25 -0500

    tested

--- 02-queens/tester.java
+++ 02-queens/tester.java
@@ -1,8 +1,30 @@
 public class tester {
 
   public static void main(String args[]) {
-    QueenBoard test = new QueenBoard(3);
-    System.out.println(test.toString());
+    QueenBoard test = new QueenBoard(8);
+    // test.solve();
+    // test.addQueen(1,2);
+    // System.out.println(test.toString());
+    // test.addQueen(0,0);
+    // test.solve();
+    // test.solve();
+    // System.out.println(test.toString());
+    // test.removeQueen(1,2);
+    System.out.println(test.countSolutions());
+    // System.out.println(test.solve());
+    // test.removeQueen(0,0);
+    // System.out.println(test.countSolutions());
+    // System.out.println(test.solve());
+    // System.out.println(test.empty());
+    // for(int i = 0; i < 10; i++) {
+    //   System.out.println(test.findQueen(i));
+    // }
+    // System.out.println(test.findQueen(0));
+    // System.out.println(test.findQueen(1));
+    // System.out.println(test.findQueen(2));
+    // System.out.println(test.findQueen(3));
+
   }
+
 
 }
