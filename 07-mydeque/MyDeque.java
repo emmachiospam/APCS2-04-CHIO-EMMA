f2b2a704f0fb635978be1be3bc7998057fd39128
emmachio <>
2021-03-20 21:47:41 -0400

    size

---
+++ 07-mydeque/MyDeque.java
@@ -0,0 +1,25 @@
+public class MyDeque<E> {
+  private E[] data;
+  private int size, start, end;
+
+  @SuppressWarnings("unchecked")
+  public MyDeque() {
+    data = (E[])new Object[10];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+
+  @SuppressWarnings("unchecked")
+  public MyDeque(int initialCapacity) {
+    data = (E[])new Object[initialCapacity];
+    size = 0;
+    start = 0;
+    end = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+  
+}
