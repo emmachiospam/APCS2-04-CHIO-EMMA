839741e3b7a9938648818e407dff73c73ba68547
emmachio <>
2021-03-30 12:00:49 -0400

    added exceptions

--- 08-calculator/Calculator.java
+++ 08-calculator/Calculator.java
@@ -2,6 +2,10 @@ import java.util.*;
 import java.io.*;
 public class Calculator{
 
+  public static void main(String[] args) {
+    System.out.println(eval("4 2 3 5 1 - + * + *"));
+  }
+
   public static double eval(String s) {
     ArrayDeque<Double> values = new ArrayDeque<Double>();
     double value1 = 0;
@@ -11,30 +15,45 @@ public class Calculator{
     while(p.hasNext()) {
       String current = p.next();
       if(current.equals("+")) {
+        if(values.size() < 2) {
+          throw new IllegalArgumentException("not enough operands");
+        }
         value1 = values.removeLast();
         value2 = values.removeLast();
         result = value1 + value2;
         values.addLast(result);
       }
       else if(current.equals("-")) {
+        if(values.size() < 2) {
+          throw new IllegalArgumentException("not enough operands");
+        }
         value1 = values.removeLast();
         value2 = values.removeLast();
         result = value2 - value1;
         values.addLast(result);
       }
       else if(current.equals("*")) {
+        if(values.size() < 2) {
+          throw new IllegalArgumentException("not enough operands");
+        }
         value1 = values.removeLast();
         value2 = values.removeLast();
         result = value1 * value2;
         values.addLast(result);
       }
       else if(current.equals("/")) {
+        if(values.size() < 2) {
+          throw new IllegalArgumentException("not enough operands");
+        }
         value1 = values.removeLast();
         value2 = values.removeLast();
         result = value2 / value1;
         values.addLast(result);
       }
       else if(current.equals("%")) {
+        if(values.size() < 2) {
+          throw new IllegalArgumentException("not enough operands");
+        }
         value1 = values.removeLast();
         value2 = values.removeLast();
         int temp = (int) Math.floor(value2 / value1);
@@ -46,7 +65,7 @@ public class Calculator{
       }
     }
     if(values.size() > 1) {
-      throw new IllegalArgumentException("not enough operands");
+      throw new IllegalArgumentException("too many operands");
     }
     else {
       return values.getFirst();
