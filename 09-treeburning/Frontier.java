6188cd3a9322ab47c94d6ab18a476a169ab52c1e
emmachio <>
2021-04-15 00:05:47 -0400

    basic methods

---
+++ 09-treeburning/Frontier.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Frontier {
+  private ArrayDeque<int[]> frontier;
+
+  public Frontier(){
+    frontier = new ArrayDeque<int[]>();
+  }
+
+  public int size() {
+    return frontier.size();
+  }
+
+  public int[] remove() {
+    return frontier.removeFirst();
+  }
+
+}
