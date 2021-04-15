d2ecc8be479826428737a06cfe8a8e4147f31e7d
emmachio <>
2021-04-15 01:24:14 -0400

    optimized

--- 09-treeburning/BurnTrees.java
+++ 09-treeburning/BurnTrees.java
@@ -6,6 +6,7 @@ public class BurnTrees{
   private static int FIRE = 1;
   private static int ASH = 3;
   private static int SPACE = 0;
+  private Frontier frontier;
 
 
   /*DO NOT UPDATE THIS
@@ -24,6 +25,7 @@ public class BurnTrees{
    */
   public BurnTrees(int width,int height, double density){
     map = new int[height][width];
+    frontier = new Frontier();
     for(int r=0; r<map.length; r++ )
       for(int c=0; c<map[r].length; c++ )
         if(Math.random() < density)
@@ -35,14 +37,12 @@ public class BurnTrees{
    *@return false if any fires are still burning, true otherwise
    */
   public boolean done(){
-    for(int i = 0; i < map.length; i++) {
-      for(int j = 0; j < map[0].length; j++) {
-        if (map[i][j] == FIRE) {
-          return false;
-        }
-      }
+    if(frontier.size() == 0) {
+      return true;
     }
-    return true;
+    else {
+      return false;
+    }
   }
 
   public boolean inRange(int r, int c) {
@@ -59,30 +59,37 @@ public class BurnTrees{
    *new fires should remain fire, and not spread.
    */
   public void tick(){
-    int[][] temp = new int[map.length][map[0].length];
-    for(int k = 0; k < map.length; k++) {
-      temp[k] = map[k].clone();
-    }
-    for(int i = 0; i < temp.length; i++) {
-      for(int j = 0; j < temp[0].length; j++) {
-        if(map[i][j] == FIRE) {
-          if(inRange(i + 1, j) && map[i + 1][j] == TREE) {
-            temp[i + 1][j] = FIRE;
-          }
-          if(inRange(i - 1, j) && map[i - 1][j] == TREE) {
-            temp[i - 1][j] = FIRE;
-          }
-          if(inRange(i , j + 1) && map[i][j + 1] == TREE) {
-            temp[i][j + 1] = FIRE;
-          }
-          temp[i][j] = ASH;
+    int size = frontier.size();
+    for(int i = 0; i < size; i++) {
+        int[] temp = frontier.remove();
+        int row = temp[0];
+        int col = temp[1];
+        if(inRange(row + 1, col) && map[row + 1][col] == TREE) {
+          map[row + 1][col] = FIRE;
+          int[] toAdd = new int[2];
+          toAdd[0] = row + 1;
+          toAdd[1] = col;
+          frontier.add(toAdd);
         }
+        if(inRange(row - 1, col) && map[row - 1][col] == TREE) {
+          map[row - 1][col] = FIRE;
+          int[] toAdd = new int[2];
+          toAdd[0] = row - 1;
+          toAdd[1] = col;
+          frontier.add(toAdd);
+        }
+        if(inRange(row, col + 1) && map[row][col + 1] == TREE) {
+          map[row][col + 1] = FIRE;
+          int[] toAdd = new int[2];
+          toAdd[0] = row;
+          toAdd[1] = col + 1;
+          frontier.add(toAdd);
+        }
+        map[row][col] = ASH;
       }
+      ticks++;
     }
-    map = temp;
-    ticks++;
     //YOU MUST IMPLEMENT THIS
-  }
 
   /*
    *Sets the trees in the left column of the forest on fire
@@ -93,6 +100,10 @@ public class BurnTrees{
     for(int i = 0; i < map.length; i++){
       if(map[i][0]==TREE){
         map[i][0]=FIRE;
+        int[] coordinate = new int[2];
+        coordinate[0] = i;
+        coordinate[1] = 0;
+        frontier.add(coordinate);
       }
     }
   }
