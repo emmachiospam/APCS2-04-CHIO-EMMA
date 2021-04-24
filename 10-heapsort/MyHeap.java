86a47404c9d5dbe01996434f461ea6d8fd1a972c
emmachio <>
2021-04-24 12:25:43 -0400

    done and tested

--- 10-heapsort/MyHeap.java
+++ 10-heapsort/MyHeap.java
@@ -36,11 +36,12 @@ public class MyHeap{
         if(child1 > parent) {
           data[2 * index + 1] = parent;
           data[index] = child1;
+          pushDown(data, size, 2 * index + 1);
         }
     }
   }
 
-  public static int howManyChildren(int size, int index) {
+  private static int howManyChildren(int size, int index) {
     if((index * 2) + 1 < size) {
       if((index * 2) + 2 < size) {
         return 2;
@@ -54,35 +55,26 @@ public class MyHeap{
     }
   }
 
-  public static void buildHeap(int[] data) {
-    for(int i = data.length-1; i >= 0; i--) {
-      pushDown(data, data.length - 1, i);
+  private static void buildHeap(int[] data) {
+    for(int i = data.length - 1; i >= 0; i--) {
+      pushDown(data, data.length, i);
     }
   }
 
-  public static void remove(int[] data, int size) {
+  private static void remove(int[] data, int size) {
     int first = data[0];
-    data[0] = data[size];
-    data[size] = first;
+    data[0] = data[size-1];
+    data[size-1] = first;
     size--;
     pushDown(data, size, 0);
   }
 
   public static void heapsort(int[] data) {
     buildHeap(data);
-    for(int i = data.length-1; i > 0; i--) {
+    System.out.println(Arrays.toString(data));
+    for(int i = data.length; i > 0; i--) {
       remove(data, i);
     }
-  }
-
-  public static void main(String args[]) {
-    int[] test = {10, 20, 4, 1, 63, 25, 73, 16};
-    System.out.println(Arrays.toString(test));
-    // buildHeap(test);
-    // System.out.println(Arrays.toString(test));
-    // remove(test, 7);
-    heapsort(test);
-    System.out.println(Arrays.toString(test));
   }
 
 }
